0b46595a2481fd098359082137bc5b59715d7a81
eddie pearson <eddieismusic@gmail.com>
2020-05-17 10:28:52 -0400

    added a p5.js sketch in click

--- app.js
+++ app.js
@@ -10,3 +10,38 @@ play.addEventListener('click', function () {
         this.classList.remove('black');
     }
 });
+
+const numShapes = 3
+const maxSize = 50
+
+let colors = []
+function setup() {
+    colors = [
+        color(210, 202, 177),
+        color(235, 231, 219),
+        color(255, 130, 130),
+        color(255, 0, 0),
+    ]
+    createCanvas(window.innerWidth, window.innerHeight)
+    noStroke()
+}
+
+function randomNumber(size) {
+    return Math.floor(Math.random() * size)
+}
+
+function randomChoice(choices) {
+    let index = randomNumber(choices.length)
+    return choices[index]
+}
+
+function mouseClicked() {
+    let sideLength = randomNumber(maxSize)
+    fill(randomChoice(colors))
+    ellipse(mouseX, mouseY, sideLength, sideLength)
+}
+
+
+window.onresize = () => {
+    resizeCanvas(window.innerWidth, window.innerHeight)
+}
